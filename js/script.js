8c0287371c00bc4689591ef62323d0c169d5bebf
Wagner da Silva Camargo Filho <144560716+wagner333@users.noreply.github.com>
2024-07-20 22:59:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ function redirecionar(url) {
     window.location.href = url;
 }
 function btnInportaten(){
-    location.href = "http://127.0.0.1:5501/projeto%200-4(links)/php/contato.html"
+    location.href = "./php/contato.html"
 }
